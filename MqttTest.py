a1d7279520b4eb28f129f2d4c7bc25d229c108c4
wenwu <xiewenwu@mychery.com>
2021-08-25 14:06:41 +0800

    change:some optimize

--- MqttTest.py
+++ MqttTest.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3.8
+#!/usr/bin/python3
 import requests
 import base64
 import json
@@ -7,9 +7,9 @@ import os
 from concurrent.futures import ThreadPoolExecutor, as_completed 
 
 class MqttTest:
-    "test mqtt some api like pubush/batchPub.."
+    "test mqtt some api like publish/batchPub.."
     # api base url
-    __baseUrl = ""
+    __baseUrl = "http://xxx.x.x.x/"
     __appKey = ""
     __appSecret=""
     #__fileHandle
@@ -36,12 +36,13 @@ class MqttTest:
         # return jsonBody
 
     def __genPatchMsg(self, index):
+        'send msg format'
         msgContent = {"topic": "T1D_TrackingReport", "payload": json.dumps({"msgId": index, "timestamp": self.__getMilliTime()}), "qos": 2, "client_id":"T18T1D123456789", "retain": False}
         return json.dumps(msgContent)
 
-    def patchPublishMsg(self, total):
-        with ThreadPoolExecutor(4) as excutors:
-            allTasks = [excutors.submit(self.publishMsg, (self.__genPatchMsg(index))) for index in range(0, total)]
+    def patchPublishMsg(self, totalMsg, threadCount):
+        with ThreadPoolExecutor(threadCount) as excutors:
+            allTasks = [excutors.submit(self.publishMsg, (self.__genPatchMsg(index))) for index in range(0, totalMsg)]
         dirStr = os.getcwd() + '/data/'
         dirExist = os.path.exists(dirStr)
         print('dir: ' + dirStr + ' exist: ' + str(dirExist))
@@ -64,15 +65,10 @@ class MqttTest:
         'get current timestamp in milliseconds'
         return int(time.time() * 1000)
 
-appKeyStr = "20f4eb16851a9"
-appSecretStr = "MzAwNjIzODEzOTM4OTE5ODY3OTczNTE1NjY4NzY1NDA5MjI"
+appKeyStr = "xxx"
+appSecretStr = "xxx"
 msgContent = {"topic": "T1D_TrackingReport", "payload": json.dumps({"msgId": 1, "timestamp": int(time.time() * 1000)}), "qos": 2, "client_id":"T18T1D123456789", "retain": False}
 msgContentStr = json.dumps(msgContent)
-# msgContentJson = json.loads(msgContentStr)
-# print(msgContentJson)
-# payloadJson = json.loads(msgContentJson['payload'])
-# print(payloadJson)
-# print('msgId: ' + str(payloadJson['msgId']) + ' timestamp: ' + str(payloadJson['timestamp']))
 apiTest = MqttTest(appKeyStr, appSecretStr)
 # apiTest.publishMsg(msgContentStr)
-apiTest.patchPublishMsg(50)
+apiTest.patchPublishMsg(10, 4)
